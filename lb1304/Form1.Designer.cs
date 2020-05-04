703a52f063f28f860670367fe38cccaa969207df
Влад Зброжек <pointbreak22@outlook.com>
2020-05-04 19:14:54 +0300

    новое готовое

---
+++ lb1304/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace lb1304
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 14);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(189, 34);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 463);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "lb1304";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ lb1304/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb1304
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Panel picture = new Panel();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32(numericUpDown1.Value);
+            picture.Size = new Size(k*10, k*10);
+            picture.Location = new Point(12, 56);
+            picture.BorderStyle = BorderStyle.FixedSingle;
+            this.Controls.Remove(picture);
+            this.Controls.Add(picture);
+
+
+            Graphics g =picture.CreateGraphics();
+
+            for (int i = (k - 1); i > 0; i--)
+            {
+                g.DrawLine(new Pen(Color.Blue), 10, i * 10, k * 10 - 10, i * 10);
+                if (i % 2 != 0)
+                {
+                    g.DrawLine(new Pen(Color.Blue), 10, i * 10, 13, i * 10+3);
+                    g.DrawLine(new Pen(Color.Blue), 10, i * 10, 13, i * 10-3);
+                    if (i != 1)
+                    {
+                        g.DrawLine(new Pen(Color.Blue), 10, i * 10, 10, (i-1) * 10 );
+                       g.DrawLine(new Pen(Color.Blue), 10, (i-1) * 10, 13, (i-1) * 10 + 3);
+                        g.DrawLine(new Pen(Color.Blue), 10, (i - 1) * 10, 7, (i - 1) * 10 + 3);
+                    }
+
+                }
+                else
+                {
+                    g.DrawLine(new Pen(Color.Blue), k * 10 - 13, i * 10+3, k * 10 - 10, i * 10);
+                    g.DrawLine(new Pen(Color.Blue), k * 10 - 13, i * 10-3, k * 10 - 10, i * 10);
+
+
+                    g.DrawLine(new Pen(Color.Blue), k * 10-10, i * 10, k * 10-10, (i - 1) * 10);
+                    g.DrawLine(new Pen(Color.Blue), k * 10 - 10, (i - 1) * 10, k * 10 - 13, (i - 1) * 10 + 3);
+                    g.DrawLine(new Pen(Color.Blue), k * 10 - 10, (i - 1) * 10, k * 10 -7, (i - 1) * 10 + 3);
+
+                }
+            }
+
+           
+
+        }
+
+    }
+}
+
