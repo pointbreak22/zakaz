657fbf146786ac3d717d3d6cb089d0ddf6a30ac5
Влад Зброжек <pointbreak22@outlook.com>
2020-05-06 15:24:33 +0300

    готовое третье

--- lb1504/Form1.Designer.cs
+++ lb1504/Form1.Designer.cs
@@ -28,48 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.zed = new ZedGraph.ZedGraphControl();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 354);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 373);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 519);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(748, 38);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Вернуть в прежнее состояние";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zed
+            // 
+            this.zed.Location = new System.Drawing.Point(13, 13);
+            this.zed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zed.Name = "zed";
+            this.zed.ScrollGrace = 0D;
+            this.zed.ScrollMaxX = 0D;
+            this.zed.ScrollMaxY = 0D;
+            this.zed.ScrollMaxY2 = 0D;
+            this.zed.ScrollMinX = 0D;
+            this.zed.ScrollMinY = 0D;
+            this.zed.ScrollMinY2 = 0D;
+            this.zed.Size = new System.Drawing.Size(748, 499);
+            this.zed.TabIndex = 2;
+            this.zed.MouseMove += new System.Windows.Forms.MouseEventHandler(this.zed_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 569);
+            this.Controls.Add(this.zed);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "lb1504";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zed;
     }
 }
 
--- lb1504/Form1.cs
+++ lb1504/Form1.cs
@@ -15,11 +15,40 @@ namespace lb1504
         public Form1()
         {
             InitializeComponent();
+            Paint();
         }
+        public void Paint()
+        {
+            zed.GraphPane.CurveList.Clear();
+            zed.GraphPane.GraphObjList.Clear();
+            zed.GraphPane.AddCurve("Line", new[] { 0.0, 0.0 }, new[] { 0.0, 4.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 0.0, 2.0 }, new[] { 4.0, 4.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 2.0, 2.0 }, new[] { 0.0, 4.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 2.0, 0.0 }, new[] { 0.0, 0.0 }, Color.Red);
+
+            zed.GraphPane.AddCurve("Line", new[] { 1.0, 4.0 }, new[] { 1.0, 1.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 1.0, 1.0 }, new[] { 1.0, 3.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 1.0, 4.0 }, new[] { 3.0, 3.0 }, Color.Red);
+            zed.GraphPane.AddCurve("Line", new[] { 4.0, 4.0 }, new[] { 1.0, 3.0 }, Color.Red);
+            zed.AxisChange();
+            zed.Invalidate();
+
+        }
+
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            flag1 = 0;
+            flag2 = 0;
+            flag3 = 0;
+
+
+
+
+            Paint();
         }
         public void FillPolygonPoint(PaintEventArgs e)
         {
@@ -41,12 +70,117 @@ namespace lb1504
             // Draw polygon to screen.
             e.Graphics.FillPolygon(blueBrush, curvePoints);
             e.Graphics.DrawPolygon(pen, curvePoints2);
-         
-        }
 
+        }
+        
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             this.FillPolygonPoint(e);
         }
+        int flag1 = 0; int flag2 = 0; int flag3 = 0;
+        private void zed_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Сюда будут записаны координаты в системе координат графика
+            double x, y;
+
+            // Пересчитываем пиксели в координаты на графике
+            // У ZedGraph есть несколько перегруженных методов ReverseTransform.
+            zed.GraphPane.ReverseTransform(e.Location, out x, out y);
+
+            // Выводим результат
+            string text = string.Format("X: {0};    Y: {1}", x, y);
+            //textBox1.Text = text;
+           
+            if ((x > 0 && x < 2 && y < 4 && y > 3) || (x > 0 && x < 1 && y < 3 && y > 1) || (x > 0 && x < 2 && y < 1 && y > 0))
+            {
+                if (flag1 == 0)
+                { 
+                var poly = new ZedGraph.PolyObj
+                {
+                    Points = new[]
+                      {
+                          new ZedGraph.PointD(0, 0),
+                          new ZedGraph.PointD(0, 4),
+                          new ZedGraph.PointD(2, 4),
+                          new ZedGraph.PointD(2, 3),
+                          new ZedGraph.PointD(1, 3),
+                          new ZedGraph.PointD(1, 1),
+                          new ZedGraph.PointD(2, 1),
+                          new ZedGraph.PointD(2, 0)
+                },
+
+                    Fill = new ZedGraph.Fill(Color.Green),
+                    ZOrder = ZedGraph.ZOrder.E_BehindCurves
+                };
+                zed.GraphPane.GraphObjList.Add(poly);
+                zed.AxisChange();
+                zed.Invalidate();
+                flag1++;
+
+               }
+
+
+            }
+            if (x > 1 && x < 2 && y < 3 && y > 1)
+            {
+                if (flag2 == 0)
+                {
+                    var poly2 = new ZedGraph.PolyObj
+                {
+                    Points = new[]
+                  {
+                          new ZedGraph.PointD(1, 1),
+                          new ZedGraph.PointD(1, 3),
+                          new ZedGraph.PointD(2, 3),
+                          new ZedGraph.PointD(2, 1)
+                       
+                },
+
+                    Fill = new ZedGraph.Fill(Color.Green),
+                    ZOrder = ZedGraph.ZOrder.E_BehindCurves
+                };
+                zed.GraphPane.GraphObjList.Add(poly2);
+                zed.AxisChange();
+                zed.Invalidate();
+                flag2++;
+
+                }
+
+
+
+            }
+            if (x <4 && x > 2 && y < 3 && y > 1)
+            {
+                if (flag3 == 0)
+                {
+                    var poly2 = new ZedGraph.PolyObj
+                {
+                    Points = new[]
+                  {                     
+                           new ZedGraph.PointD(2, 1),
+                           new ZedGraph.PointD(2, 3),
+                           new ZedGraph.PointD(4, 3),
+                           new ZedGraph.PointD(4, 1),
+                },
+
+                    Fill = new ZedGraph.Fill(Color.Green),
+                    ZOrder = ZedGraph.ZOrder.E_BehindCurves
+                };
+                zed.GraphPane.GraphObjList.Add(poly2);
+                zed.AxisChange();
+                zed.Invalidate();
+                flag3++;
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+
     }
 }
