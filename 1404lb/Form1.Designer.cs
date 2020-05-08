42c8be45417d1d5de329db588387b9ff94034773
Влад Зброжек <pointbreak22@outlook.com>
2020-05-08 14:04:21 +0300

    готовое 4е

---
+++ 1404lb/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _1404lb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(6, 51);
+            this.zedGraph.Margin = new System.Windows.Forms.Padding(17, 16, 17, 16);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(550, 350);
+            this.zedGraph.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Нарисовать график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(70, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 38);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 409);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraph);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ 1404lb/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace _1404lb
+{
+    public partial class Form1 : Form
+    {
+        GraphPane pane;
+        public Form1()
+        {
+            InitializeComponent();
+            pane = zedGraph.GraphPane;
+            pane.Title.Text = "y=x^2/A";
+            pane.XAxis.Title.Text = "Ось X";
+            pane.YAxis.Title.Text = "Ось Y";
+          
+
+        }
+        private double f(double x, double a)
+        {
+
+            return Math.Pow(x,2)/a;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+    
+
+            // Очистим список кривых на тот случай, если до этого сигналы уже были нарисованы
+            pane.CurveList.Clear();
+          
+            // Создадим список точек
+            PointPairList list = new PointPairList();
+          
+            int a = Convert.ToInt32(numericUpDown1.Value);
+            double xmin = -500;
+            double xmax = 500;
+
+            // Заполняем список точек
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                // добавим в список точку
+                list.Add(x, f(x,a));
+            }
+
+            // Создадим кривую с названием "Sinc",
+            // которая будет рисоваться голубым цветом (Color.Blue),
+            // Опорные точки выделяться не будут (SymbolType.None)
+            LineItem myCurve = pane.AddCurve("Парабола", list, Color.Blue, SymbolType.None);
+
+            // Вызываем метод AxisChange (), чтобы обновить данные об осях.
+            // В противном случае на рисунке будет показана только часть графика,
+            // которая умещается в интервалы по осям, установленные по умолчанию
+            zedGraph.AxisChange();
+
+            // Обновляем график
+            zedGraph.Invalidate();
+        }
+    }
+}
